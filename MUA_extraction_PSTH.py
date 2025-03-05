7ea86640d256a8bcb27a4d5dd5906ea237dfe02f
Kai Lun <kt1524@my.bristol.ac.uk>
2025-03-05 19:16:32 +0100

    Add files via upload

--- MUA_extraction_PSTH.py
+++ MUA_extraction_PSTH.py
@@ -17,7 +17,7 @@ import MUA_STA_RFs as mua
 recordingPath = '.../20250224'
 stim_ttl_dir = os.path.join(recordingPath, "Openephys2025-02-24_16-21-03/Record Node 103/experiment1/Openephys_Opto_NPX-SC/events/NI-DAQmx-101.PXIe-6341/TTL")
 raw_data_dir = os.path.join(recordingPath, "Openephys2025-02-24_16-21-03/Record Node 103/experiment1/Openephys_Opto_NPX-SC/continuous/Neuropix-PXI-100.ProbeA-AP")
-probe_ttl_dir_path = os.path.join(recordingPath, "Openephys2025-02-24_16-21-03/Record Node 103/experiment1/Openephys_Opto_NPX-SC/events/Neuropix-PXI-100.ProbeA-AP/TTL")
+probe_ttl_dir = os.path.join(recordingPath, "Openephys2025-02-24_16-21-03/Record Node 103/experiment1/Openephys_Opto_NPX-SC/events/Neuropix-PXI-100.ProbeA-AP/TTL")
 save_dir = recordingPath
 
 
@@ -41,7 +41,7 @@ pix_data = mua.extract_NP_MUA(
     align_to_probe_timestamps=True,  # If True, the stim TTLs in stim_ttl_dir will be aligned to NP probe timestamps (sync TTLs in probe_ttl_dir).
     stim_sync_ch=1,  # The channel state for the sync channel of the stimulus TTLs.
     probe_sync_ch=1,  # The channel state for the sync channel of the probe TTLs.
-    probe_ttl_dir= probe_ttl_dir_path,  # The folder path of the probe TTLs.
+    probe_ttl_dir=probe_ttl_dir,  # The folder path of the probe TTLs.
     n_cores=int(os.cpu_count()/2)  # The number of CPUs to be used for parallel computing. Spare some CPUs so that the computer is not slowed down for other processes.
 )
 
